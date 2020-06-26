54cec162028f73050a750853987bfd1810ef6730
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-26 19:56:34 +0300

    Resources used instead of bare strings and colors

--- app/src/main/java/android/development/mynotes/MainnavActivity.java
+++ app/src/main/java/android/development/mynotes/MainnavActivity.java
@@ -81,7 +81,8 @@ public class MainnavActivity extends AppCompatActivity
     private void initializeDisplayContent() {
         mRecyclerItems = findViewById(R.id.list_items);
         mNotesLayoutManager = new LinearLayoutManager(this);
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        mCoursesLayoutManager = new GridLayoutManager(this,
+                getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
